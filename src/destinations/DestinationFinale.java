0052810e3342f773e5eba0542753ca5befd69b5b
Antoine FAVREL <antoine.favrel@imt-atlantique.net>
2023-08-31 11:53:58 +0000

    Update DestinationFinale.java

--- src/destinations/DestinationFinale.java
+++ src/destinations/DestinationFinale.java
@@ -3,10 +3,11 @@ package destinations;
 import information.Information;
 import information.InformationNonConformeException;
 
-public class DestinationFinale extends Destination<Boolean>{
-
-    @Override
-    public void recevoir(Information<Boolean> information) throws InformationNonConformeException {
-
-    }
+public class DestinationFinale extends Destination<Boolean> {
+	public void recevoir(Information<Boolean> information) throws InformationNonConformeException {	
+		informationRecue = new Information<Boolean>();
+		for(int i=0; i<information.nbElements(); i++) {
+			informationRecue.add((Boolean)information.iemeElement(i));
+		}
+	}
 }
