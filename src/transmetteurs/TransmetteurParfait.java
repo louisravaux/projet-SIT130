5f4da454f92cfeb0a49321a45eaacb55ae8e574b
Antoine FAVREL <antoine.favrel@imt-atlantique.net>
2023-08-31 11:53:22 +0000

    Update TransmetteurParfait.java

--- src/transmetteurs/TransmetteurParfait.java
+++ src/transmetteurs/TransmetteurParfait.java
@@ -1,20 +1,25 @@
 package transmetteurs;
 
+import destinations.DestinationInterface;
 import information.Information;
 import information.InformationNonConformeException;
 
-public class TransmetteurParfait extends Transmetteur<Boolean, Boolean>{
-    public TransmetteurParfait() {
+public class TransmetteurParfait extends Transmetteur<Boolean,Boolean> {
+	public void recevoir(Information<Boolean> information) throws InformationNonConformeException {
+		informationRecue = new Information<Boolean>();
+		for(int i=0; i<information.nbElements(); i++) {
+			informationRecue.add((Boolean)information.iemeElement(i));
+		}
+		emettre();
+	}
 
-    }
-
-    @Override
-    public void recevoir(Information<Boolean> information) throws InformationNonConformeException {
-
-    }
-
-    @Override
-    public void emettre() throws InformationNonConformeException {
-
-    }
+	public void emettre() throws InformationNonConformeException {
+		informationEmise = new Information<Boolean>();
+		for(int i=0; i<informationRecue.nbElements(); i++) {
+			informationEmise.add((Boolean)informationRecue.iemeElement(i));
+		}
+		for (DestinationInterface <Boolean> destinationConnectee : destinationsConnectees) {
+            destinationConnectee.recevoir(informationEmise);
+		}
+	}
 }
