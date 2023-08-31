c0e2fb5ec913ba9a6ff38d913694b3d124f3b588
Antoine FAVREL <antoine.favrel@imt-atlantique.net>
2023-08-31 11:52:06 +0000

    Add SourceAleatoire.java

---
+++ src/sources/SourceAleatoire.java
@@ -0,0 +1,16 @@
+package sources;
+
+import information.Information;
+import java.util.Random;
+
+public class SourceAleatoire extends Source<Boolean> {
+	/** Une source qui envoie un message aleatoire */
+    public SourceAleatoire() {
+        informationGeneree = new Information<Boolean>();
+        Random rd = new Random();
+        for(int i=0; i<6; i++) {
+        	boolean bool = rd.nextBoolean();
+        	informationGeneree.add(bool);
+        }
+    }
+}
