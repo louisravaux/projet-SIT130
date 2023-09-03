3c1164fa290dc9bb766da078ee6288ec957e99e8
Louis Ravaux <louis.ravaux@imt-atlantique.net>
2023-09-03 18:39:59 +0200

    minor changes on SourceAleatoire

--- src/sources/SourceAleatoire.java
+++ src/sources/SourceAleatoire.java
@@ -4,13 +4,18 @@ import information.Information;
 import java.util.Random;
 
 public class SourceAleatoire extends Source<Boolean> {
+    
 	/** Une source qui envoie un message aleatoire */
     public SourceAleatoire() {
+
         informationGeneree = new Information<Boolean>();
+
         Random rd = new Random();
-        for(int i=0; i<6; i++) {
+        for(int i = 0; i < 6; i++) {
+
         	boolean bool = rd.nextBoolean();
         	informationGeneree.add(bool);
+
         }
     }
 }
