1d75dfbb0851ded9cee770ba4119d2e5372e964f
Louis Ravaux <louis.ravaux@imt-atlantique.net>
2023-09-05 15:44:46 +0200

    javadoc SourceFixe

--- src/sources/SourceFixe.java
+++ src/sources/SourceFixe.java
@@ -2,8 +2,16 @@ package sources;
 
 import information.Information;
 
+/**
+ * La classe SourceFixe représente une source qui génère toujours le même message, composé de valeurs booléennes prédéfinies.
+ * Elle hérite de la classe Source et génère un signal fixe lors de sa création.
+ */
 public class SourceFixe extends Source<Boolean> {
-	/** Une source qui envoie toujours le m�me message */
+
+    /**
+     * Constructeur de la classe SourceFixe.
+     * Il initialise l'information générée avec un message fixe constitué de valeurs booléennes prédéfinies.
+     */
     public SourceFixe() {
         informationGeneree = new Information<Boolean>();
         informationGeneree.add(true);
@@ -13,5 +21,5 @@ public class SourceFixe extends Source<Boolean> {
         informationGeneree.add(false);
         informationGeneree.add(true);
     }
-    
 }
+
