7c0802765b2cd3d773850494a0c6fda576269ba5
Antoine FAVREL <antoine.favrel@imt-atlantique.net>
2023-09-08 09:55:27 +0000

    Update EmetteurParfaitAnalogique.java v0

--- src/emetteur/EmetteurParfaitAnalogique.java
+++ src/emetteur/EmetteurParfaitAnalogique.java
@@ -1,1 +1,20 @@
+package emetteur;
 
+import destinations.DestinationInterface;
+import information.Information;
+import information.InformationNonConformeException;
+
+public class EmetteurParfaitAnalogique extends Emetteur<Double, Double> {
+	public void recevoir(Information<Double> information) throws InformationNonConformeException {
+		for (Double i : information) {
+            informationRecue.add(i);
+        }
+	}
+	
+	public void emettre() throws InformationNonConformeException {
+		for (DestinationInterface<Double> destinationConnectee : destinationsConnectees) {
+            destinationConnectee.recevoir(informationRecue);
+        }
+		this.informationEmise = informationRecue;
+	}
+}
