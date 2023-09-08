b379f5971e4738e5e90053a2e11e0a6eb204b3d6
Antoine FAVREL <antoine.favrel@imt-atlantique.net>
2023-09-08 09:54:39 +0000

    Add new file recepteur.java

---
+++ src/recepteur/Recepteur.java
@@ -0,0 +1,47 @@
+package recepteur;
+
+import java.util.LinkedList;
+
+import destinations.DestinationInterface;
+import information.Information;
+import information.InformationNonConformeException;
+import sources.SourceInterface;
+
+public abstract class Recepteur <R,E> implements  DestinationInterface <R>, SourceInterface <E> {
+	protected LinkedList <DestinationInterface <E>> destinationsConnectees;
+	protected Information <R>  informationRecue;
+	protected Information <E>  informationEmise;
+	protected int vmax;
+	protected int vmin;
+	
+	
+	public Recepteur() {
+		destinationsConnectees = new LinkedList <DestinationInterface <E>> ();
+		informationRecue = null;
+		informationEmise = null;
+		vmax = 0;
+		vmin = 0;
+	}
+	
+	public Information <R>  getInformationRecue() {
+		return this.informationRecue;
+	}
+	
+	public Information <E>  getInformationEmise() {
+		return this.informationEmise;
+	}
+	
+	public void connecter (DestinationInterface <E> destination) {
+		destinationsConnectees.add(destination); 
+	}
+	
+	public void deconnecter (DestinationInterface <E> destination) {
+		destinationsConnectees.remove(destination); 
+	}
+	
+	public  abstract void recevoir(Information <R> information) throws InformationNonConformeException;
+	
+	public  abstract void emettre() throws InformationNonConformeException;
+	
+	
+}
