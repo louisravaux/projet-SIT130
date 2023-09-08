f4929d37b73af7b7cf9ebebb1a91e278cf074823
Antoine FAVREL <antoine.favrel@imt-atlantique.net>
2023-09-08 09:56:47 +0000

    Update RecepteurParfaitAnalogique.java v0

--- src/recepteur/RecepteurParfaitAnalogique.java
+++ src/recepteur/RecepteurParfaitAnalogique.java
@@ -1,1 +1,20 @@
+package recepteur;
 
+import destinations.DestinationInterface;
+import information.Information;
+import information.InformationNonConformeException;
+
+public class RecepteurParfaitAnalogique extends Recepteur<Double, Double> {
+	public void recevoir(Information<Double> information) throws InformationNonConformeException {
+		for (Double i : information) {
+            informationRecue.add(i);
+        }
+	}
+	
+	public void emettre() throws InformationNonConformeException {
+		for (DestinationInterface<Double> destinationConnectee : destinationsConnectees) {
+            destinationConnectee.recevoir(informationRecue);
+        }
+		this.informationEmise = informationRecue;
+	}
+}
