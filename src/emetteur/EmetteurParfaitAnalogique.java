91dd033b8a6e45ee8f9af925a5abf97da37006e9
Antoine FAVREL <antoine.favrel@imt-atlantique.net>
2023-09-11 17:00:23 +0000

    Update EmetteurParfaitAnalogique.java

--- src/emetteur/EmetteurParfaitAnalogique.java
+++ src/emetteur/EmetteurParfaitAnalogique.java
@@ -5,10 +5,31 @@ import information.Information;
 import information.InformationNonConformeException;
 
 public class EmetteurParfaitAnalogique extends Emetteur<Double, Double> {
+	private float amin;
+	private float amax;
+	private int nb_samples;
+	private String form;
+	
+	public EmetteurParfaitAnalogique(float amin, float amax, int nb_samples, String form) {
+		
+		// TODO verifications des vars a faire
+		
+		this.amin = amin;
+		this.amax = amax;
+		this.nb_samples = nb_samples;
+		this.form = form;
+	}
+	
 	public void recevoir(Information<Double> information) throws InformationNonConformeException {
-		for (Double i : information) {
-            informationRecue.add(i);
-        }
+		if(form.equals("RZ")) {
+			// TODO 
+		}
+		if(form.equals("NRZ")) {
+			// TODO 
+		}
+		if(form.equals("NRZT")) {
+			// TODO 
+		}
 	}
 	
 	public void emettre() throws InformationNonConformeException {
