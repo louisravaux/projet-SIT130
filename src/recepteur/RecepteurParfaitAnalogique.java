6b3159ee889e27e3d481130cb9118c634730ef1f
Antoine FAVREL <antoine.favrel@imt-atlantique.net>
2023-09-11 17:03:16 +0000

    Update RecepteurParfaitAnalogique.java

--- src/recepteur/RecepteurParfaitAnalogique.java
+++ src/recepteur/RecepteurParfaitAnalogique.java
@@ -5,16 +5,36 @@ import information.Information;
 import information.InformationNonConformeException;
 
 public class RecepteurParfaitAnalogique extends Recepteur<Double, Double> {
+	private float amin;
+	private float amax;
+	private int nb_samples;
+	private String form;
+	
+	public RecepteurParfaitAnalogique(float amin, float amax, int nb_samples, String form) {
+		
+		// TODO verifications des vars a faire
+		
+		this.amin = amin;
+		this.amax = amax;
+		this.nb_samples = nb_samples;
+		this.form = form;
+	}
+	
 	public void recevoir(Information<Double> information) throws InformationNonConformeException {
-		for (Double i : information) {
-            informationRecue.add(i);
-        }
+		if(form.equals("RZ")) {
+			// TODO 
+		}
+		if(form.equals("NRZ")) {
+			// TODO 
+		}
+		if(form.equals("NRZT")) {
+			// TODO 
+		}
 	}
 	
 	public void emettre() throws InformationNonConformeException {
 		for (DestinationInterface<Double> destinationConnectee : destinationsConnectees) {
-            destinationConnectee.recevoir(informationRecue);
+            destinationConnectee.recevoir(informationEmise);
         }
-		this.informationEmise = informationRecue;
 	}
 }
