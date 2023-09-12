24869155b64d0a7513ce8bcb9e742a8b1242c908
Antoine FAVREL <antoine.favrel@imt-atlantique.net>
2023-09-12 18:28:38 +0000

    Update EmetteurParfaitAnalogique.java : RZ NRZ v0

--- src/emetteur/EmetteurParfaitAnalogique.java
+++ src/emetteur/EmetteurParfaitAnalogique.java
@@ -4,36 +4,75 @@ import destinations.DestinationInterface;
 import information.Information;
 import information.InformationNonConformeException;
 
-public class EmetteurParfaitAnalogique extends Emetteur<Boolean, Double> {
-	private float amin;
-	private float amax;
+public class EmetteurParfaitAnalogique extends Emetteur<Boolean, Float> {
+	private float vmin;
+	private float vmax;
 	private int nb_samples;
 	private String form;
 	
-	public EmetteurParfaitAnalogique(float amin, float amax, int nb_samples, String form) {
+	public EmetteurParfaitAnalogique(float vmin, float vmax, int nb_samples, String form) {
 		
 		// TODO verifications des vars a faire
 		
-		this.amin = amin;
-		this.amax = amax;
+		this.vmin = vmin;
+		this.vmax = vmax;
 		this.nb_samples = nb_samples;
 		this.form = form;
 	}
 	
+	
 	public void recevoir(Information<Boolean> information) throws InformationNonConformeException {
+		int signalLength = informationRecue.nbElements() * nb_samples * 2; 
+		float[] rzSignal = new float[signalLength];
+		int index = 0;
+		
 		if(form.equals("RZ")) {
-			// TODO 
+			for(Boolean b : informationRecue) {
+				for(int i=0; i<nb_samples; i++) {
+					if(b) {
+						rzSignal[index++] = vmax;
+					}
+					else {
+						rzSignal[index++] = vmin;
+					}
+				}
+				for(int i=0; i<nb_samples; i++) {
+					if(b) {
+						rzSignal[index++] = 0.0f;
+					}
+					else {
+						rzSignal[index++] = 0.0f;
+					}
+				}
+			}
 		}
 		if(form.equals("NRZ")) {
-			// TODO 
+			for(Boolean b : informationRecue) {
+				for(int i=0; i<nb_samples; i++) {
+					if(b) {
+						rzSignal[index++] = vmax;
+					}
+					else {
+						rzSignal[index++] = vmin;
+					}
+				}
+				for(int i=0; i<nb_samples; i++) {
+					if(b) {
+						rzSignal[index++] = vmax;
+					}
+					else {
+						rzSignal[index++] = vmin;
+					}
+				}
+			}
 		}
 		if(form.equals("NRZT")) {
 			// TODO 
-		}
+		}	
 	}
 	
 	public void emettre() throws InformationNonConformeException {
-		for (DestinationInterface<Double> destinationConnectee : destinationsConnectees) {
+		for (DestinationInterface<Float> destinationConnectee : destinationsConnectees) {
             destinationConnectee.recevoir(informationEmise);
         }
 	}
