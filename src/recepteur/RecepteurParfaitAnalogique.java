f75517238f8019615ce448fc82913b0b3b02d1d8
Antoine FAVREL <antoine.favrel@imt-atlantique.net>
2023-09-13 20:39:35 +0000

    omg leak ça marche ?

--- src/recepteur/RecepteurParfaitAnalogique.java
+++ src/recepteur/RecepteurParfaitAnalogique.java
@@ -4,36 +4,47 @@ import destinations.DestinationInterface;
 import information.Information;
 import information.InformationNonConformeException;
 
-public class RecepteurParfaitAnalogique extends Recepteur<Double, Double> {
-	private float amin;
-	private float amax;
+public class RecepteurParfaitAnalogique extends Recepteur<Float, Boolean> {
+	private float vmin;
+	private float vmax;
 	private int nb_samples;
 	private String form;
 	
-	public RecepteurParfaitAnalogique(float amin, float amax, int nb_samples, String form) {
-		
-		// TODO verifications des vars a faire
-		
-		this.amin = amin;
-		this.amax = amax;
+	public RecepteurParfaitAnalogique(float vmin, float vmax, int nb_samples, String form) {
+		this.vmin = vmin;
+		this.vmax = vmax;
 		this.nb_samples = nb_samples;
 		this.form = form;
+		informationRecue = new Information<>(); // float
+		informationEmise = new Information<>(); // boolean
 	}
 	
-	public void recevoir(Information<Double> information) throws InformationNonConformeException {
-		if(form.equals("RZ")) {
-			// TODO 
-		}
-		if(form.equals("NRZ")) {
-			// TODO 
-		}
-		if(form.equals("NRZT")) {
-			// TODO 
+	public void recevoir(Information<Float> information) throws InformationNonConformeException {
+		int j = 0;
+		int k = 0;
+		float somme = 0;
+		float esperance = (vmin + vmax) / 2;
+		
+		if(form.equals("RZ") || form.equals("NRZ") || form.equals("NRZT")) {
+			for (Float f : informationRecue) {
+				j++;
+				somme += f;
+				if(j == nb_samples) {
+					if(somme > esperance) {
+						informationEmise.add(true);
+					}
+					else {
+						informationEmise.add(false);
+						j = 0;
+						somme = 0;
+					}
+				}
+			} 
 		}
 	}
 	
 	public void emettre() throws InformationNonConformeException {
-		for (DestinationInterface<Double> destinationConnectee : destinationsConnectees) {
+		for (DestinationInterface<Boolean> destinationConnectee : destinationsConnectees) {
             destinationConnectee.recevoir(informationEmise);
         }
 	}
