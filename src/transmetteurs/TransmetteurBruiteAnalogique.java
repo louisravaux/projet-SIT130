e87ec29502d095ed68148e9d1d5f3213dd46d7dc
Antoine FAVREL <antoine.favrel@imt-atlantique.net>
2023-09-14 09:30:14 +0000

    Update TransmetteurBruiteAnalogique.java : v0 with noise

--- src/transmetteurs/TransmetteurBruiteAnalogique.java
+++ src/transmetteurs/TransmetteurBruiteAnalogique.java
@@ -1,1 +1,55 @@
+package transmetteurs;
 
+import java.util.Random;
+
+import destinations.DestinationInterface;
+import information.Information;
+import information.InformationNonConformeException;
+
+public class TransmetteurBruiteAnalogique extends Transmetteur<Float, Float> {
+	private float snr;
+	
+	public TransmetteurBruiteAnalogique(float snr) {
+        this.snr = (float) Math.pow(10, snr/10);
+		informationRecue = new Information<>();
+        informationEmise = new Information<>();
+    }
+
+    @Override
+    public void recevoir(Information<Float> information) throws InformationNonConformeException {
+    	// vars
+    	float p_signal = 0f;
+    	float p_noise = 0f;
+    	float sigma_noise = 0f;
+    	Random a1 = new Random();
+        Random a2 = new Random();
+        
+        // Calcul puissance du signal
+        for (Float i : informationRecue) {
+    		p_signal += Math.pow(i, 2);
+        }
+        p_signal /= informationRecue.nbElements();
+        
+        // Calcul puissance du bruit
+        p_noise = p_signal/snr;
+        
+        // Calcul sigma
+        sigma_noise = (float) Math.sqrt(p_noise);
+        
+        // Generation du bruit gaussien
+        for (Float i : information) {
+    		i += (float) (sigma_noise*Math.sqrt(-2*Math.log(1-a1.nextFloat()))*Math.cos(2*Math.PI*a2.nextFloat()));
+    		informationRecue.add(i);
+        }
+        
+        emettre();
+    }
+
+    @Override
+    public void emettre() throws InformationNonConformeException {
+        for (DestinationInterface<Float> destinationConnectee : destinationsConnectees) {
+            destinationConnectee.recevoir(informationRecue);
+        }
+        this.informationEmise = informationRecue;
+    }
+}
