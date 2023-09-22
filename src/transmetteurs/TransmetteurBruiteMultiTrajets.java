e4f0eae3941543ee1520bd9bbb7b0bdba494191f
Antoine FAVREL <antoine.favrel@imt-atlantique.net>
2023-09-22 13:15:56 +0200

    new TransmetteurBruiteMultiTrajets v1

--- src/transmetteurs/TransmetteurBruiteMultiTrajets.java
+++ src/transmetteurs/TransmetteurBruiteMultiTrajets.java
@@ -3,22 +3,49 @@ package transmetteurs;
 import java.util.ArrayList;
 import java.util.Random;
 
+import com.sun.source.tree.ArrayAccessTree;
 import destinations.DestinationInterface;
 import information.Information;
 import information.InformationNonConformeException;
 
 public class TransmetteurBruiteMultiTrajets extends TransmetteurBruiteAnalogique {
 
+    ArrayList<Float> tau;
+    ArrayList<Float> a;
     /**
      * Constructeur de la classe TransmetteurBruiteAnalogique.
      *
      * @param snr
      * @param nb_sample
      */
-    public TransmetteurBruiteMultiTrajets(float snr, int nb_sample) {
+    public TransmetteurBruiteMultiTrajets(float snr, int nb_sample, ArrayList<Float> tau, ArrayList<Float> a) {
         super(snr, nb_sample);
+        this.tau = tau;
+        this.a = a;
     }
 
+    public void addMultiTrajets(Information<Float> information) {
+        for(Float i : information) {
+            for(Float t : tau) {
+                //multiX = a*;
+                //informationEmise.add(i+multiX);
+            }
+        }
+    }
 
+    @Override
+    public void recevoir(Information<Float> information) throws InformationNonConformeException {
+        super.recevoir(information);
 
+        generateNoise(information);
+
+        for(Float i : information) {
+
+        }
+    }
+
+    @Override
+    public void emettre() throws InformationNonConformeException {
+        super.emettre();
+    }
 }
