ab50a5e237b67016324197acc21bd094a5a28054
Antoine FAVREL <antoine.favrel@imt-atlantique.net>
2023-10-08 15:26:22 +0200

    new : RecepteurParfaitAnalogiqueTest created

--- src/tests/RecepteurParfaitAnalogiqueTest.java
+++ src/tests/RecepteurParfaitAnalogiqueTest.java
@@ -1,5 +1,32 @@
 package tests;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 public class RecepteurParfaitAnalogiqueTest {
 
+    @BeforeEach
+    void init() throws Exception {
+
+    }
+
+    @Test
+    public void testReceiveRZ() {
+
+    }
+
+    @Test
+    public void testReceiveNRZ() {
+
+    }
+
+    @Test
+    public void testReceiveNRZT() {
+
+    }
+
+    @Test
+    public void testRecevoir() {
+
+    }
 }
