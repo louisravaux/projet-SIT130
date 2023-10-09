a955852eac2c458d6826c129fe8abeb231b1d3da
Antoine FAVREL <antoine.favrel@imt-atlantique.net>
2023-10-09 10:25:59 +0200

    new : RecepteurParfaitAnalogiqueTest created

--- src/tests/RecepteurParfaitAnalogiqueTest.java
+++ src/tests/RecepteurParfaitAnalogiqueTest.java
@@ -1,34 +1,59 @@
 package tests;
 
+import information.Information;
+import information.InformationNonConformeException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import recepteur.RecepteurParfaitAnalogique;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class RecepteurParfaitAnalogiqueTest {
+    RecepteurParfaitAnalogique r;
+    Information<Float> infos;
 
 
 
     @BeforeEach
     void init() throws Exception {
-
+        r = new RecepteurParfaitAnalogique(0f, 1f, 6, "RZ");
+        infos = new Information<>(); infos.add(0.2f); infos.add(0.8f); infos.add(0.7f); infos.add(0.9f); infos.add(0.1f); infos.add(0.5f);
     }
 
     @Test
-    public void testReceiveRZ() {
-
+    public void testReceiveRZ() throws InformationNonConformeException {
+        r.recevoir(infos);
+        r.receiveRZ();
+        Information<Boolean> informationEmise = r.getInformationEmise();
+        assertEquals(2, informationEmise.nbElements());
+        assertEquals(true, informationEmise.iemeElement(0));
+        assertEquals(true, informationEmise.iemeElement(1));
     }
 
     @Test
-    public void testReceiveNRZ() {
-
+    public void testReceiveNRZ() throws InformationNonConformeException {
+        r.recevoir(infos);
+        r.receiveNRZ();
+        Information<Boolean> informationEmise = r.getInformationEmise();
+        assertEquals(2, informationEmise.nbElements());
+        assertEquals(true, informationEmise.iemeElement(0));
+        assertEquals(true, informationEmise.iemeElement(1));
     }
 
     @Test
-    public void testReceiveNRZT() {
-
+    public void testReceiveNRZT() throws InformationNonConformeException {
+        r.recevoir(infos);
+        r.receiveNRZT();
+        Information<Boolean> informationEmise = r.getInformationEmise();
+        assertEquals(2, informationEmise.nbElements());
+        assertEquals(true, informationEmise.iemeElement(0));
+        assertEquals(true, informationEmise.iemeElement(1));
     }
 
     @Test
-    public void testRecevoir() {
-
+    public void testRecevoir() throws InformationNonConformeException {
+        Information<Float> information = new Information<>(); information.add(0.2f); information.add(0.8f); information.add(0.5f); information.add(0.7f); information.add(0.9f); information.add(0.1f);
+        r.recevoir(information);
+        assertEquals(information, r.getInformationRecue());
     }
 }
